e31493471e66f9fbf8004e94e89e9e726c08039b
a.alekseev <a.alekseev@netris.ru>
2020-09-12 08:49:23 +0300

    Initial commit

---
+++ src/test/java/ru/alarh/InfoResourceTest.java
@@ -0,0 +1,21 @@
+package ru.alarh;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class InfoResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hello"));
+    }
+
+}
